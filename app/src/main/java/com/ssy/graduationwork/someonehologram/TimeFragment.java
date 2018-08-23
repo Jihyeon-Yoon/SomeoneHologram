7b9b3da89438921c68abaf14eb7cf2aade8d278e
YJH <altos0120@hanmail.net>
2018-08-23 20:17:23 +0900

    Add Clock & Date

--- app/src/main/java/com/ssy/graduationwork/someonehologram/TimeFragment.java
+++ app/src/main/java/com/ssy/graduationwork/someonehologram/TimeFragment.java
@@ -7,9 +7,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 
 public class TimeFragment extends Fragment {
+
+    private TextView dateDisplay;
 
 
     public TimeFragment() {
@@ -20,7 +26,20 @@ public class TimeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_time, container, false);
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_time, null);
+
+        dateDisplay = rootView.findViewById(R.id.date);
+        dateDisplay.setText(this.getDate());
+
+
+
+        return rootView;
+    }
+
+    private String getDate() {
+        String currentDateTimeString = DateFormat.getDateInstance().format(new Date());
+
+        return currentDateTimeString;
     }
 
 
